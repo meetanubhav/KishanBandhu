ac2d27deaa6f6bd7128820fa64e783dec1ccfda3
Anubhav Gupta <37771611+meetmonster@users.noreply.github.com>
2019-02-15 15:17:21 +0530

    updates

--- Dashboard/availableLand.php
+++ Dashboard/availableLand.php
@@ -2,6 +2,13 @@
 <html>
 <head>
 	<title> </title>
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
+
 </head>
 <body>
 	<div class="row">
@@ -19,14 +26,28 @@
 				$llord=mysqli_fetch_assoc($r);
 				$lid=$llord['lid'];
 				$lr=mysqli_query($con,"select * from addland where lid='$lid'");
+				?>
+				<div class="" id>
+				<?php
 				if ($arr1=mysqli_fetch_assoc($lr))
 				{
 					echo $arr1['tot'];		
 					echo "string";			
 				}
-
-
 			?>
+		</div>
+			<button class="btn btn-primary btn-sm" data-toggle="modal" data-target="#myModal"><span class="fa fa-plus"></span></button>
+			div class="modal fade" id="myModal">
+    <div class="modal-dialog modal-sm">
+      <div class="modal-content">
+        
+        <!-- Modal body -->
+        <div class="modal-body">
+          Modal body..
+        </div>
+      </div>
+    </div>
+  </div>
 		</div>
 		<div class="col-md-3"></div>		
 	</div>
--- Dashboard/farmerHome.php
+++ Dashboard/farmerHome.php
@@ -27,7 +27,19 @@ session_start();
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-    <script src="../assets/js/farmer.js"></script>
+    <!-- <script src="../assets/js/farmer.js"></script> -->
+     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script>
+$(document).ready(function(){
+    $("#farmerContent").hide();
+  $("#hide").click(function(){
+    $("#farmerContent").hide();
+  });
+  $("#show").click(function(){
+    $("#farmerContent").show();
+  });
+});
+</script>
 </head>
 
 <body>
@@ -75,12 +87,13 @@ session_start();
             <div class="col-md-6 col-sm-12">
                 <div class="card">
                     <div class="card-body text-center" id="farmerContent">
+
                         <?php
-                         if($arr1=mysqli_fetch_assoc($lr))
-                    {
-                    echo $arr1['tot'];      
-                    echo "string";          
-                        }
+                         // if($arr1=mysqli_fetch_assoc($lr))
+                    // {
+                    // echo $arr1['tot'];      
+                    // echo "string";          
+                        // }
 
 
             ?>
