65bf230e0ab4b0255206ca1536ca12664c564775
Anubhav Gupta <37771611+meetmonster@users.noreply.github.com>
2019-02-19 21:22:01 +0530

    Update expertHome.php

--- Dashboard/expertHome.php
+++ Dashboard/expertHome.php
@@ -33,6 +33,41 @@
     <!-- home start -->
     <div class="container-fluid">
 
+        <form action="upload.php" method="POST" enctype="multipart/form-data">
+            <div class="row">
+            <div class="col-md-4 panel panel-body panel-default">
+                <br>
+                <div class="upload-image">
+                    <h3>Upload Land Image</h3>
+                    <div class="upload-image-preview"></div>
+                    <input class="" type="file" name="fileToUpload" id="fileToUpload">  
+                    </div>
+            </div>
+            <!-- <div class="col-md-2" id=""></div> -->
+            <div class="col-md-6" id="formbox">
+
+            <div class="form-group">
+                <input type="text" class="form-control" id="name" name="name" placeholder="Name" required="required">
+              </div>
+              <div class="form-group">
+                <input type="text" class="form-control" id="username" name="username" placeholder="Username" required="required">
+              </div>
+              <div class="form-group">
+                <input type="text" class="form-control" id="uid" name="uid" placeholder="Aadhar/Ration card/Green Card" required="required">
+              </div>
+               <div class="form-group">
+                <input type="text" class="form-control" id="city" name="city" placeholder="City" required="required">
+              </div>
+               <div class="form-group">
+                <input type="text" class="form-control" id="district" name="district" placeholder="District" required="required">
+              </div>              
+              <div class="form-group">
+                <input type="checkbox" name="terms" class="control-label"> I agree to the <a href="terms.html"><u>Terms and Conditions.</u></a>
+            </div>
+            <input class="btn btn-primary" type="submit" name="register" value="Submit" placeholder="Submit">
+            </div>
+        </form>
+
     </div>
 </body>
 </html>
